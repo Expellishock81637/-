5750ee8e1d9a240179074be02dc68b37659061f9
st1254878 <70092046+st1254878@users.noreply.github.com>
2023-12-25 01:50:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const express = require('express');
 const app = express();
-
+const cors = require('cors'); 
 app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 // 引入Router (routes目錄底下的todo.js)
 const studentsRouter = require('./routes/students');
 app.use('/students', studentsRouter);
